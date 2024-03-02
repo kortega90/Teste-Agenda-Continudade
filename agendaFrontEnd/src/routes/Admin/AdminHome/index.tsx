7a639e94a2b8d15e01737f1b6c18e27768381787
Kengi Ortega <kortega90@hotmail.com>
2024-03-02 12:21:04 -0300

    Correção de dependencias

--- agendaFrontEnd/src/routes/Admin/AdminHome/index.tsx
+++ agendaFrontEnd/src/routes/Admin/AdminHome/index.tsx
@@ -1,10 +1,30 @@
+
 import './styles.css'
+import * as userService from "../../../services/user-Service";
+import { useEffect, useState } from 'react';
+import { UserDTO } from '../../../models/user';
+
 export default function AdminHome() {
+
+  const [user, setUser] = useState<UserDTO>();
+
+  useEffect(() => {
+    userService.findMe()
+      .then(response => {
+        setUser(response.data);
+        console.log(response.data);
+        console.log(user);
+      })
+      .catch( error => {
+        console.log("Error", error);
+      });
+  }, [user]);
+
   return (
     <main>
       <section id="admin-home-section" className ="dsc-container">
         <h2 className ="dsc-section-title dsc-mb20">
-          Bem-vindo à área administrativa
+          Bem-vindo à área administrativa {user?.name}
         </h2>
       </section>
     </main>
